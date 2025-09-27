c0771b2ba37c4dfeadb33f41d4a750ad87c87359
doawelul <dfang314@gmail.com>
2025-09-26 23:20:57 -0500

    some qol, including retry on failed parse

--- flower_collection.py
+++ flower_collection.py
@@ -7,7 +7,7 @@ from PIL import Image
 STARTUP_TIME = 3 # seconds it takes before the script starts
 
 IMAGE_RESIZE = 3 # ratio of resizing to help image parsing
-SCREENSHOT_REGION = (165, 128, 143, 14) # location of left, top, width, height of the coordinates on the screen
+SCREENSHOT_REGION = (166, 128, 143, 14) # location of left, top, width, height of the coordinates on the screen
 
 FLOWER_PATH = [(507.09, 449.30),
                (507.40, 398.33),
@@ -20,59 +20,83 @@ FLOWER_PATH = [(507.09, 449.30),
                (458.15, 366.63),
                (486.50, 297.94)]
 
-FLOWER_HARVEST_TIME = 1.75 # time to harvest a flower
+FLOWER_HARVEST_TIME = 1.4 # time to harvest a flower
 
+# These globals are related to calibration. Some are not usable until calibration is complete
 CALIBRATION_SEC = 0.4 # how long to move for w and a during calibration
 W_MOVE_SEC = (None, None) # x, z change for 1 second of holding w
 A_MOVE_SEC = (None, None) # x, z change for 1 second of holding a
 
+GET_POS_TRIES = 3 # amount of times to try get_pos until you crash
+JIGGLE_TIME = 0.01 # time to move to change position when retrying get_pos
 GET_POS_WAIT_TIME = 0.1 # amount of time to wait for coordinates to stabilize
 
-def hold_key(key, duration):
-  pydirectinput.keyDown(key)
+def hold_keys(keys, duration):
+  for key in keys:
+    pydirectinput.keyDown(key)
   time.sleep(duration)
-  pydirectinput.keyUp(key)
+  for key in keys:
+    pydirectinput.keyUp(key)
+
+def ordinal(x):
+    suffix = 'th'
+    if (x % 100) < 11 or (x % 100) > 13:
+        suffix = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][x % 10]
+    return str(x) + suffix
 
 def get_pos():
   time.sleep(GET_POS_WAIT_TIME)
-  im = pyautogui.screenshot(region=SCREENSHOT_REGION)
 
-  # coordinates are bright green, filter into black onto white text
-  width, height = im.size
-  im = im.resize((width*IMAGE_RESIZE, height*IMAGE_RESIZE), resample=Image.BILINEAR)
-  width, height = im.size
-  for x in range(width):
-    for y in range(height):
-      r, g, b = im.getpixel((x, y))
-      if g > 170 and r < 170 and b < 170:
-        im.putpixel((x, y), (0, 0, 0))
-      else:
-        im.putpixel((x, y), (255, 255, 255))
-  # through trial and error this config is goated
-  # output in format
-  # mmm.mmynnnn.nn*ooo.oo
-  # where mmm.mm is the negative x, nnnn.nn is the y, * is some character (most of the time z) and ooo.oo is the negative z
-  coordstr = pytesseract.image_to_string(im, config="--psm 7 -c tessedit_char_whitelist=0123456789yz.")
-  coordstr = coordstr[:-1] # parser always gives a newline at the end
-  pos = float(coordstr[:6]), float(coordstr[-6:])
-  print("Current position", pos)
-  return pos
+  for try_number in range(1, GET_POS_TRIES + 1):
+    im = pyautogui.screenshot(region=SCREENSHOT_REGION)
+
+    # coordinates are bright green, filter into black onto white text
+    width, height = im.size
+    im = im.resize((width*IMAGE_RESIZE, height*IMAGE_RESIZE), resample=Image.BILINEAR)
+    width, height = im.size
+    for x in range(width):
+      for y in range(height):
+        r, g, b = im.getpixel((x, y))
+        if g > 180 and r < 100 and b < 100:
+          im.putpixel((x, y), (0, 0, 0))
+        else:
+          im.putpixel((x, y), (255, 255, 255))
+    
+    # through trial and error this config is goated
+    # output in format mmmmmynnnnnn*ooooo
+
+    # mmm[.]mm is the negative x,
+    # nnnn[.]nn is the y,
+    # * is some character (most of the time z),
+    # ooo[.]oo is the negative z
+    coordstr = pytesseract.image_to_string(im, config="--psm 7 -c tessedit_char_whitelist=0123456789yz")
+    coordstr = coordstr[:-1] # parser always gives a newline at the end
+    try:
+      pos = float(coordstr[:5]) / 100, float(coordstr[-5:]) / 100
+      print("Current position", pos)
+      return pos
+    except ValueError:
+      print(f"Failed to parse coordinates {coordstr}. This is the {ordinal(try_number)} fail in a row.")
+      hold_keys(["w", "a"], JIGGLE_TIME)
+      continue
+  print("Max amount of retries reached and still couldn't parse coordinates.")
+  raise ValueError(f"Couldn't parse coordinates")
 
 def calibrate():
   global W_MOVE_SEC, A_MOVE_SEC
   print("Calibrating...")
 
   startx, startz = get_pos()
-  hold_key("w", CALIBRATION_SEC)
+  hold_keys(["w"], CALIBRATION_SEC)
   endx, endz = get_pos()
   W_MOVE_SEC = (endx - startx) / CALIBRATION_SEC, (endz - startz) / CALIBRATION_SEC
-  hold_key("s", CALIBRATION_SEC)
+  hold_keys(["s"], CALIBRATION_SEC)
 
   startx, startz = get_pos()
-  hold_key("a", CALIBRATION_SEC)
+  hold_keys(["a"], CALIBRATION_SEC)
   endx, endz = get_pos()
   A_MOVE_SEC = (endx - startx) / CALIBRATION_SEC, (endz - startz) / CALIBRATION_SEC
-  hold_key("d", CALIBRATION_SEC)
+  hold_keys(["d"], CALIBRATION_SEC)
 
 def cycle():
   print("Starting a cycle")
@@ -98,10 +122,14 @@ def cycle():
     ws_key = "w" if amt_w > 0 else "s"
     ad_key = "a" if amt_a > 0 else "d"
 
-    hold_key(ws_key, abs(amt_w))
-    hold_key(ad_key, abs(amt_a))
+    amt_w = abs(amt_w)
+    amt_a = abs(amt_a)
 
-    hold_key("e", FLOWER_HARVEST_TIME)
+    hold_keys([ws_key], amt_w)
+    hold_keys([ad_key], amt_a)
+
+    # new flower needs to get pos so we can start that while still harvesting this flower
+    hold_keys(["e"], FLOWER_HARVEST_TIME - GET_POS_WAIT_TIME) 
   
 pyautogui.FAILSAFE = True
 
