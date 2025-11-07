93e02fb527b391da0ba758470294aa48e84da7e1
doawelul <dfang314@gmail.com>
2025-11-06 19:33:07 -0600

    Start on comp finder for merge tactics

---
+++ comp_finder.py
@@ -0,0 +1,18 @@
+# This is a team comp calculator for merge tactics for when you have the target dummy.
+
+traits = {"clan",
+          "brawler",
+          "noble",
+          "blaster",
+          "electric",
+          "wizard",
+          "juggernaut",
+          "ace",
+          "avenger",
+          "goblin",
+          "ghost",
+          "assassin",
+          "archer",
+          "fire"}
+
+units = {} # units["name"] = cost, trait1, trait2
